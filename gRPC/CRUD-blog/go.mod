bd4bb1d7a82c96c9eaef58254675844d63b9d383
Igor Cheliadinski <icheliadinski@gmail.com>
2019-12-21 12:56:50 -0500

    add: create stubs

--- gRPC/CRUD-blog/go.mod
+++ gRPC/CRUD-blog/go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/golang/protobuf v1.3.2
 	github.com/jessevdk/go-flags v1.4.0 // indirect
+	go.mongodb.org/mongo-driver v1.2.0 // indirect
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
 	golang.org/x/sys v0.0.0-20191220220014-0732a990476f // indirect
 	golang.org/x/text v0.3.2 // indirect
--- gRPC/CRUD-blog/server/main.go
+++ gRPC/CRUD-blog/server/main.go
@@ -1,7 +1,49 @@
 package main
 
+import (
+	"context"
+	blogpb "crud-proto/proto"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
 type BlogServiceServer struct{}
 
-func main() {
+func (s *BlogServiceServer) CreateBlog(ctx context.Context, req *blogpb.CreateBlogReq) (*blogpb.CreateBlogRes, error) {
+	return nil, nil
+}
 
+func (s *BlogServiceServer) ReadBlog(ctx context.Context, req *blogpb.ReadBlogReq) (*blogpb.ReadBlogRes, error) {
+	return nil, nil
+}
+
+func (s *BlogServiceServer) UpdateBlog(ctx context.Context, req *blogpb.UpdateBlogReq) (*blogpb.UpdateBlogRes, error) {
+	return nil, nil
+}
+
+func (s *BlogServiceServer) DeleteBlog(ctx context.Context, req *blogpb.DeleteBlogReq) (*blogpb.DeleteBlogRes, error) {
+	return nil, nil
+}
+
+func (s *BlogServiceServer) ListBlog(ctx context.Context, req *blogpb.ListBlogReq) (*blogpb.ListBlogRes, error) {
+	return nil, nil
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	fmt.Println("Starting server on port :50051...")
+
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Unable to listen on port :50051: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+
+	s := grpc.NewServer(opts...)
+	srv := &BlogServiceServer{}
+
+	blogpb.RegisterBlogServiceServer(s, srv)
 }
