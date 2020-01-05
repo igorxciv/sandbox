d1c1bd54b1a119b1cec771c01e8a7a3cb6d3e09c
Igor Cheliadinski <icheliadinski@gmail.com>
2020-01-05 12:34:38 -0500

    add: form handling

--- docker/multi-container/client/src/Fib.js
+++ docker/multi-container/client/src/Fib.js
@@ -1,6 +1,22 @@
 import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 
+const SeenIndexes = ({ indexes }) => {
+  return indexes.map(({ number }) => number).join(", ");
+};
+
+const CalculatedValues = ({ values }) => {
+  const entries = [];
+  for (const key in values) {
+    entries.push(
+      <div key={key}>
+        For index {key} I calculated {values[key]}
+      </div>
+    );
+  }
+  return entries;
+};
+
 export const Fib = () => {
   const [seenIndexes, setSeenIndexes] = useState([]);
   const [values, setValues] = useState({});
@@ -16,10 +32,31 @@ export const Fib = () => {
     setSeenIndexes(seen);
   };
 
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    await axios.post("/api/values", { index });
+    setIndex("");
+  };
+
   useEffect(() => {
     fetchValues();
     fetchIndexes();
   }, []);
 
-  return null;
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your index:</label>
+        <input onChange={e => setIndex(e.target.value)} value={index} />
+        <button>Submit</button>
+      </form>
+
+      <h3>Indexes I have seen</h3>
+      <SeenIndexes indexes={seenIndexes} />
+
+      <h3>Calculated values</h3>
+      <CalculatedValues values={values} />
+    </div>
+  );
 };
