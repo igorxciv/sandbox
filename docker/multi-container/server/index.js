d16c023cba6810d9479beb5bdabfdac25a69c281
Igor Cheliadinski <icheliadinski@gmail.com>
2020-01-05 12:12:39 -0500

    add: express routes configuration

--- docker/multi-container/server/index.js
+++ docker/multi-container/server/index.js
@@ -23,6 +23,45 @@ pgClient.on("error", () => {
   console.log("Lost PG connection");
 });
 
-pgClient.query("CREATE TABLE IF NOT EXISTS values (number INT)").catch(err => {
+pgClient.query("CREATE TABLE IF NOT EXISTS values (number INT);").catch(err => {
   console.log(err);
 });
+
+// Redis configuration
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+app.get("/", (req, res) => {
+  return res.send("hi");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * FROM values;");
+  return res.send(values.rows);
+});
+
+app.get("/values/current", (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    if (err) return err;
+    return res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body["index"];
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index is too high");
+  }
+  redisClient.hset("values", index, "Nothing yet!");
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES $1", [index]);
+
+  return res.send({ working: true });
+});
+
+app.listen(5000, err => console.log("Listening..."));
