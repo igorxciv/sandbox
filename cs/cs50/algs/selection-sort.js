b0066f18112cdfd042cf722344337298a7b41349
Igor Cheliadinski <icheliadinski@gmail.com>
2020-01-28 22:22:06 -0500

    add: selection sort

---
+++ cs/cs50/algs/selection-sort.js
@@ -0,0 +1,18 @@
+function selection_sort(vector) {
+  for (let i = 0, l = vector.length; i < l; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < l; j++) {
+      if (vector[minIdx] > vector[j]) {
+        minIdx = j
+      }
+    }
+    if (minIdx !== i) {
+      let temp = vector[i];
+      vector[i] = vector[minIdx];
+      vector[minIdx] = temp;
+    }
+  }
+  return vector;
+}
+
+console.log(selection_sort([8, 4, 1, 3, 9, 5, 2, 7, 6]));
